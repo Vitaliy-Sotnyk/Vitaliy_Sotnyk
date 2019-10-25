f3ea46e6faf051b61e0a8219600c9808f9f1aaf1
Vitaliy-Sotnyk <otaman.19.20.70@gmail.com>
2019-10-25 21:48:02 +0300

    Lab3: запустив сервер, щоб переконатись що всі сторінки доступні

--- Lab3/main/views.py
+++ Lab3/main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import os
+from datetime import datetime
 
-# Create your views here.
+def main(request):
+    return render(request, 'main.html', {'parameter': "test"})
+
+def health(request):
+    response = {'date': datetime.now(), 'current_page': request.path, 'server_info': os.uname().sysname, 'client_info': request.META['HTTP_USER_AGENT']}
+    return JsonResponse(response)
