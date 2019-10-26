bebb528d2e477a210009f8c04d00d869cf424742
Vitaliy-Sotnyk <otaman.19.20.70@gmail.com>
2019-10-26 13:45:10 +0300

    Lab3:виконавав моніторинг

---
+++ Lab3/monitoring.py
@@ -0,0 +1,42 @@
+import requests
+import json
+import logging
+import time
+
+logging.basicConfig(
+    filename="server.log",
+    filemode='a',
+    level=logging.INFO,
+    format='{levelname} {asctime} {name} : {message}',
+    style='{'
+)
+log = logging.getLogger(__name__)
+
+
+def main(url):
+ while True:
+  time.sleep(60)
+  try:
+   r = requests.get(url)
+   r.raise_for_status()
+  except HTTPError as http_err:
+   logging.error("HTTP помилка: %s", http_err)
+  except Exception as err:
+   logging.error("Інша помилка: %s", err)
+  else:
+   if r.status_code == 200:
+    data = json.loads(r.content)
+    logging.info("Сервер доступний. Час на сервері: %s", data['date'])
+    logging.info("======================================")
+    logging.info("Запитувана сторінка: : %s", data['current_page'])
+    logging.info("Cтатус сервера: %s", data['server_info'])
+    logging.info("Cтатус кієнтa: %s", data['client_info'])
+   elif r.status_code == 404:
+    logging.warn("Cторінку не знайдено %s", r.status_code)
+   else:
+    logging.error("Сервер недоступний!!!")
+
+
+if __name__ == '__main__':
+ main("http://localhost:8000/health")
+
